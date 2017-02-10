55de484a16b965c740595798c1542a7f9254cfe8
Terry Appleby <tappleby@industrialagency.ca>
2017-02-09 23:52:09 -0500

    Add support for fluent includes, fields, sorts and pagination query params

---
+++ lib/wicket-sdk/query_builder.rb
@@ -0,0 +1,126 @@
+require 'wicket-sdk/document'
+
+module WicketSDK
+  class QueryBuilder
+    def initialize(client, route)
+      @client = client
+      @route = route
+      @query_params = {
+        include: [],
+        fields: [],
+        filter: {},
+        sort: [],
+        page: {}
+      }
+    end
+
+    def all
+      jsonapi_request(:get, @route, to_params)
+    end
+
+    def find(resource_id)
+      jsonapi_request(:get, "#{@route}/#{resource_id}", to_params)
+    end
+
+    # Set pagination settings
+    # @return [WicketSDK::QueryBuilder] self for fluent queries
+    def page(number, size = nil)
+      @query_params[:page][:number] = number
+      @query_params[:page][:size] = size if size
+      self
+    end
+
+    # Set pagination settings
+    # @return [WicketSDK::QueryBuilder] self for fluent queries
+    def includes(*args)
+      @query_params[:include] += args
+      self
+    end
+
+    def fields(*args)
+      @query_params[:fields] += args
+      self
+    end
+
+    def filter(filters = {})
+      @query_params[:filter].merge!(filters)
+      self
+    end
+
+    def sort(*args)
+      @query_params[:sort] += args
+      self
+    end
+
+    # @return [String] query as a query string
+    def to_query_string
+      Faraday::Utils.build_nested_query(to_params)
+    end
+
+    def to_s
+      to_query_string
+    end
+
+    def to_params
+      params = {}
+      params[:include] = include_to_params unless invalid_param? :include
+      params[:fields] = fields_to_params unless invalid_param? :fields
+      params[:filter] = @query_params[:filter] unless invalid_param? :filter
+      params[:sort] = sort_to_params unless invalid_param? :sort
+      params[:page] = @query_params[:page] unless invalid_param? :page
+      params
+    end
+
+    protected
+
+    def invalid_param?(param)
+      @query_params[param].empty?
+    end
+
+    def include_to_params
+      @query_params[:include].flatten.map! do |s|
+        s.to_s.strip
+      end.join(',')
+    end
+
+    def fields_to_params
+      params = @query_params[:fields].each_with_object({}) do |field_hash, fields|
+        next unless field_hash.is_a? Hash
+
+        field_hash.each do |filter, filter_value|
+          fields[filter] ||= []
+          fields[filter] << filter_value
+        end
+      end
+
+      params.map do |filter, filter_value|
+        [filter, filter_value.join(',')]
+      end.to_h
+    end
+
+    def sort_to_params
+      @query_params[:sort].flatten.map do |value|
+        if value.is_a? Hash
+          value.map do |field, dir|
+            dir.to_sym == :desc ? "-#{field}" : field
+          end
+        else
+          value.to_s
+        end
+      end.join(',')
+    end
+
+    def jsonapi_request(*args)
+      document_from_response @client.connection.run(*args)
+    end
+
+    def document_from_response(response)
+      Document.new(@client.resource_class_mappings).tap do |rs|
+        # Support for async faraday requests
+        response.on_complete do
+          rs.parse!(response.body || {})
+        end
+      end
+    end
+  end
+end
---
+++ test/wicket-sdk/query_builder_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class WicketSDK::QueryBuilderTest < Minitest::Test
+  attr_reader :query
+
+  before do
+    @query = WicketSDK::QueryBuilder.new(nil, 'path')
+  end
+
+  it 'can set page details' do
+    expected = { page: { number: 2 } }
+    assert_equal expected, query.page(2).to_params
+
+    expected = { page: { number: 3, size: 10 } }
+    assert_equal expected, query.page(3, 10).to_params
+  end
+
+  it 'can include relationships' do
+    expected = { include: 'rel1,rel2,rel3' }
+    assert_equal expected, query.includes('rel1', :rel2).includes('rel3').to_params
+  end
+
+  it 'can handle fields' do
+    query
+      .fields(res1: :field1)
+      .fields(res2: [:field4], res1: :field2)
+      .fields(res1: [:field3])
+
+    expected = {
+      fields: {
+        res1: 'field1,field2,field3',
+        res2: 'field4'
+      }
+    }
+    assert_equal expected, query.to_params
+  end
+
+  it 'can handle filters' do
+    query
+      .filter(name_eq: :field1, age_gte: 21)
+      .filter(age_lt: 30)
+
+    expected = {
+      filter: {
+        name_eq: :field1,
+        age_gte: 21,
+        age_lt: 30
+      }
+    }
+
+    assert_equal expected, query.to_params
+  end
+
+  it 'can handle sorting' do
+    query
+      .sort(:field1, field2: :desc)
+      .sort(field3: :asc)
+
+    expected = {
+      sort: 'field1,-field2,field3'
+    }
+
+    assert_equal expected, query.to_params
+  end
+end
