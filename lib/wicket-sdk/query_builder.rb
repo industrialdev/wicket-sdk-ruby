cda9496c1cfb0fb04c35d651ac38f12b3c7ba60d
Andrew Markle <andrew.markle@gmail.com>
2017-03-06 12:29:38 -0500

    Save takes json by default

--- lib/wicket-sdk/query_builder.rb
+++ lib/wicket-sdk/query_builder.rb
@@ -23,8 +23,8 @@ module WicketSDK
     end
 
     # sdk.addresses.save(org.data.addresses.primary.first)
-    def save(resource)
-      jsonapi_request(:patch, "#{@route}/#{resource.id}", "data"=> resource.to_json)
+    def save(json_resource)
+      jsonapi_request(:patch, "#{@route}/#{json_resource['id']}", "data"=> json_resource)
     end
 
     # Set pagination settings
